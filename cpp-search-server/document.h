61001356d121a393d591d1eb55d2bcf894383491
Aleksandr Rudikov <rudikoviskander@gmail.com>
2022-03-12 13:00:30 +0700

    Добавьте файлы проекта.

---
+++ cpp-search-server/document.h
@@ -0,0 +1,21 @@
+﻿//Вставьте сюда своё решение из урока «‎Очередь запросов».‎
+#pragma once
+#include <iostream>
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
+struct Document {
+    Document() = default;
+    Document(int id, double relevance, int rating);
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+std::ostream& operator<<(std::ostream& os, const Document& doc);
---
+++ cpp-search-server/read_input_functions.h
@@ -0,0 +1,6 @@
+﻿//Вставьте сюда своё решение из урока «‎Очередь запросов».‎
+#pragma once
+#include <string>
+
+std::string ReadLine();
+int ReadLineWithNumber();
