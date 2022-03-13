8edc7986d8aabcf4b1a1de6693edd640de993e1a
Aleksandr Rudikov <rudikoviskander@gmail.com>
2022-03-13 14:29:04 +0300

    Sprint 4 version 2

---
+++ search-server/document.cpp
@@ -0,0 +1,14 @@
+﻿//Вставьте сюда своё решение из урока «‎Очередь запросов».‎
+#include "document.h"
+
+Document::Document(int id, double relevance, int rating)
+    : id(id)
+    , relevance(relevance)
+    , rating(rating) {
+}
+
+std::ostream& operator<<(std::ostream& os, const Document& doc) {
+    os << "{ document_id = " << doc.id << ", relevance = " << doc.relevance
+        << ", rating = " << doc.rating << " }";
+    return os;
+}
